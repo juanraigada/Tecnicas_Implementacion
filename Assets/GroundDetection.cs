7c7a2be443c4e800176466dd57246f4a6c4ac772
Unknown <juanraigada@yahoo.com>
2017-09-28 18:36:02 +0200

    class 1

---
+++ Assets/GroundDetection.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundDetection : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+    [SerializeField]
+    LayerMask groundLayers;
+
+    [SerializeField]
+    float skinValue = 0.2f;
+    [SerializeField]
+    float separationRay = 0.2f;
+
+    bool inGround = false;
+    public bool InGround { get { return inGround; } }
+
+    Vector3 normal = Vector3.up;
+    public Vector3 Normal { get { return normal; } }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        RaycastHit2D hit1 = Physics2D.Raycast(transform.position + normal * 0.5f + Vector3.Cross(normal, Vector3.forward) * separationRay, -normal, 0.5f + skinValue, groundLayers);
+        RaycastHit2D hit2 = Physics2D.Raycast(transform.position + normal * 0.5f - Vector3.Cross(normal, Vector3.forward) * separationRay, -normal, 0.5f + skinValue, groundLayers);
+        inGround = hit1.collider != null || hit2.collider != null;
+        if (inGround)
+        {
+            if(hit1.collider != null && hit2.collider == null) 
+                normal = hit1.normal;
+            else if(hit1.collider == null && hit2.collider != null)
+                normal = hit2.normal;
+            else
+            {
+                float angle1 = Vector3.Angle(hit1.normal, Vector3.up);
+                float angle2 = Vector3.Angle(hit2.normal, Vector3.up);
+                if(angle1 > angle2)
+                    normal = hit1.normal;
+                else
+                    normal = hit2.normal;
+            }
+        }
+        else
+            normal = Vector3.up;
+        Debug.DrawLine(transform.position, transform.position + normal, Color.green); 
+        Debug.DrawLine(
+            transform.position + normal * 0.5f + Vector3.Cross(normal, Vector3.forward) * separationRay,
+            transform.position + normal * 0.5f + Vector3.Cross(normal, Vector3.forward) * separationRay - normal * (0.5f + skinValue),
+            Color.blue
+            );
+        Debug.DrawLine(
+            transform.position + normal * 0.5f - Vector3.Cross(normal, Vector3.forward) * separationRay,
+            transform.position + normal * 0.5f - Vector3.Cross(normal, Vector3.forward) * separationRay - normal * (0.5f + skinValue),
+            Color.blue
+            );
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+    [SerializeField]
+    Rigidbody2D body;
+    [SerializeField]
+    GroundDetection detector;
+    [SerializeField]
+    float gravityUp;
+    [SerializeField]
+    float gravityDown;
+    [SerializeField]
+    float maxAngle;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        CheckJump();
+        CheckGravity();
+        HorizontalMovement();
+    }
+
+    [SerializeField]
+    float maxVel;
+    [SerializeField]
+    float accel;
+    [SerializeField]
+    float horizontalDrag;
+    [SerializeField]
+    float accel_air;
+    [SerializeField]
+    float horizontalDrag_air;
+
+    [SerializeField]
+    float jumpVel;
+    [SerializeField]
+    float jumpNormalStrength;
+
+    void CheckJump()
+    {
+        if(detector.InGround && Input.GetKey(KeyCode.Space))
+        {
+            Vector3 nr = (normal.normalized * jumpNormalStrength + Vector3.up).normalized;
+            Vector3 velocityOnNormal = Vector3.Project(body.velocity, nr);
+            body.velocity -= (Vector2)velocityOnNormal;
+            body.velocity += (Vector2)nr * jumpVel;
+        }
+    }
+
+    void HorizontalMovement()
+    {
+        Vector3 newRight = Vector3.Cross(detector.Normal, Vector3.forward);
+        Vector3 velocityOnNormal = Vector3.Project(body.velocity,detector.Normal);
+        Vector3 velocityOnright = (Vector3)body.velocity - velocityOnNormal;
+        if (detector.InGround)
+        {
+
+            velocityOnright *= horizontalDrag;
+            velocityOnright += accel * Time.deltaTime * newRight * Input.GetAxis("Horizontal");
+        }
+        else
+        {
+
+            velocityOnright *= horizontalDrag_air;
+            velocityOnright += accel_air * Time.deltaTime * newRight * Input.GetAxis("Horizontal");
+        }
+        velocityOnright = Vector3.ClampMagnitude(velocityOnright, maxVel);
+        body.velocity = velocityOnright + velocityOnNormal;
+    }
+
+    Vector3 normal;
+
+    void CheckGravity()
+    {
+        float gravityToUse = gravityDown;
+        if (detector.InGround || body.velocity.y > 0)
+        {
+            gravityToUse = gravityUp;
+        }
+        normal = detector.Normal;
+        if (Vector3.Angle(normal, Vector3.up) > maxAngle)
+            normal = Vector3.up;
+        body.velocity += (Vector2)(-normal * Time.deltaTime * gravityToUse);
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, normal);
+    }
+}
